47f47f198e62b913a8728d9b8088bbca93574d34
AkseliA <61878488+AkseliA@users.noreply.github.com>
2020-04-09 19:38:23 +0300

    Multilanguage

--- Week11/app/src/main/java/com/example/week11/MainActivity.java
+++ Week11/app/src/main/java/com/example/week11/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -17,12 +23,19 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle drawerToggle;
     NavigationView navigationView;
+    TextView unEditTxt;
+    TextView displayTxt;
+    EditText editText;
     View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        unEditTxt = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        displayTxt = findViewById(R.id.display_txt);
 
         drawerLayout = findViewById(R.id.activity_main);
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.Open, R.string.Close);
@@ -48,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         if(drawerToggle.onOptionsItemSelected(item))
             return true;
 
@@ -57,6 +69,83 @@ public class MainActivity extends AppCompatActivity {
 
     public void loadActivity(View v){
         Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-        startActivity(intent);
+
+
+        startActivityForResult(intent, 1);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+
+                //Retreive values from settings activity
+                String fontsize = data.getStringExtra("fontSz");
+                String fontColor = data.getStringExtra("fontClr");
+                String width = data.getStringExtra("width");
+                String height = data.getStringExtra("height");
+                boolean lockStatus = data.getBooleanExtra("switch_boolean", false);
+                String name = data.getStringExtra("display_name");
+
+
+                //Change attributes
+                setAttributes(fontsize, fontColor, width, height, lockStatus, name);
+            }
+        }
+    }
+
+    public void setAttributes(String size, String color, String width, String height, boolean lockStatus, String name){
+
+        if(!size.equals("Current")){
+            unEditTxt.setTextSize(Integer.parseInt(size));
+        }
+
+        if(!color.equals("Current")){
+            //Change color to desired one
+            if(color.equals("White")){
+                unEditTxt.setTextColor(Color.WHITE);
+            }else if(color.equals("Black")){
+                unEditTxt.setTextColor(Color.BLACK);
+            }else if(color.equals("Yellow")){
+                unEditTxt.setTextColor(Color.YELLOW);
+            }else if(color.equals("Green")){
+                unEditTxt.setTextColor(Color.GREEN);
+            }else if(color.equals("Red")){
+                unEditTxt.setTextColor(Color.RED);
+            }else if(color.equals("Blue")){
+                unEditTxt.setTextColor(Color.BLUE);
+            }
+        }
+
+        if(!width.equals("0")){
+            ViewGroup.LayoutParams params =  unEditTxt.getLayoutParams();
+            int w = Integer.parseInt(width);
+            w = dpToPx(w);
+            params.width = w;
+            unEditTxt.setLayoutParams(params);
+        }
+
+        if(!height.equals("0")){
+            ViewGroup.LayoutParams params = unEditTxt.getLayoutParams();
+            int h = Integer.parseInt(height);
+            h = dpToPx(h);
+            params.height = h;
+            unEditTxt.setLayoutParams(params);
+        }
+        if(lockStatus){
+            editText.setEnabled(false);
+            unEditTxt.setText(editText.getText());
+            editText.setText("");
+        }else if(!lockStatus){
+            editText.setEnabled(true);
+            unEditTxt.setText(R.string.random_string);
+        }if(!name.equals("")){
+            displayTxt.setText(getString(R.string.hello) + name);
+        }
+    }
+
+
+    public int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
     }
 }
--- Week11/app/src/main/java/com/example/week11/SettingsActivity.java
+++ Week11/app/src/main/java/com/example/week11/SettingsActivity.java
@@ -2,13 +2,35 @@ package com.example.week11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
+
+import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
+    Locale locale;
     Spinner colorSpnr;
     Spinner fontSpnr;
+    Spinner languageSpnr;
+    SeekBar widthSb;
+    SeekBar heightSb;
+    Switch lockSwitch;
+    EditText name_txt;
+    int width, height;
+    int maxWidth = 400, minWidth = 150;
+    int maxHeight = 300, minHeight = 100;
+    int step = 10;
+    boolean switchState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +41,71 @@ public class SettingsActivity extends AppCompatActivity {
 
         colorSpnr = findViewById(R.id.colorSpinner);
         fontSpnr = findViewById(R.id.fontSpinner);
+        languageSpnr = findViewById(R.id.languageSpinner);
+        widthSb = findViewById(R.id.widthSB);
+        heightSb = findViewById(R.id.heightSB);
+        lockSwitch = findViewById(R.id.lock_switch);
+        name_txt = findViewById(R.id.settings_display_txt);
+
+
+
+        lockSwitch.setChecked(false);
 
 
         setSpinners();
 
+        //Language change listener
+        languageSpnr.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 1){
+                    setLocale("fi");
+                }
+                if(position == 2){
+                    setLocale("en");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //Seekbar max values
+        widthSb.setMax((maxWidth-minWidth)/step);
+        heightSb.setMax((maxHeight-minHeight)/step);
+
+        //WidthSeekbar Listener
+        widthSb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                width = minWidth + (progress*step);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        //Height Seekbar listener
+        heightSb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                height = minHeight + (progress*step);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
 
 
     }
@@ -36,5 +119,41 @@ public class SettingsActivity extends AppCompatActivity {
         ArrayAdapter fontsizeAdapter = ArrayAdapter.createFromResource(this, R.array.fontsize_array, android.R.layout.simple_spinner_item);
         fontsizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         fontSpnr.setAdapter(fontsizeAdapter);
+
+        //Language spinner
+        ArrayAdapter languageAdapter = ArrayAdapter.createFromResource(this, R.array.language_array, android.R.layout.simple_spinner_item);
+        languageAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpnr.setAdapter(languageAdapter);
     }
+
+
+
+    @Override
+    public void onBackPressed(){
+        String fontsize = fontSpnr.getSelectedItem().toString();
+        String fontcolor = colorSpnr.getSelectedItem().toString();
+        String name = name_txt.getText().toString();
+        switchState = lockSwitch.isChecked();
+
+        Intent intent = new Intent();
+        intent.putExtra("fontSz", fontsize);
+        intent.putExtra("fontClr", fontcolor);
+        intent.putExtra("height", Integer.toString(height));
+        intent.putExtra("width", Integer.toString(width));
+        intent.putExtra("switch_boolean", switchState);
+        intent.putExtra("display_name", name);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+    public void setLocale(String lang){
+        locale = new Locale(lang);
+        Resources resources = getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        Configuration config = resources.getConfiguration();
+        config.locale = locale;
+        resources.updateConfiguration(config, dm);
+        Intent refresh = new Intent(this, SettingsActivity.class);
+        startActivity(refresh);
+    }
+
 }
